00d414261fc7158ae6d883ebbb0f19db9debba5f
ZiyingResearchNotes <2022151620247@stu.scu.edu.cn>
2026-04-24 23:14:24 +0800

    Add Google Analytics tracking

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,3 +1,4 @@
+
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -7,6 +8,25 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "Ziying Research Notes",
   description: "记录临床、科研与成长中的思考",
-
+head: [
+  [
+    "script",
+    {
+      async: true,
+      src: "https://www.googletagmanager.com/gtag/js?id=G-WLTQW91H3Y",
+    },
+  ],
+  [
+    "script",
+    {},
+    `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-WLTQW91H3Y');
+    `,
+  ],
+],
   theme,
+  
 });
